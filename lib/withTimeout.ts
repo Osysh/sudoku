8d893f239669e43f61a67d8c96e2b8d8da5c85e5
Maxime <mabylon@meteomatics.com>
2026-04-06 18:18:14 +0200

    Fix connection

--- lib/withTimeout.ts
+++ lib/withTimeout.ts
@@ -1,10 +1,10 @@
-export async function withTimeout<T>(promise: Promise<T>, timeoutMs: number, label: string): Promise<T> {
+export async function withTimeout<T>(promise: PromiseLike<T>, timeoutMs: number, label: string): Promise<T> {
   return await new Promise<T>((resolve, reject) => {
     const timeoutId = setTimeout(() => {
       reject(new Error(`${label} timed out after ${timeoutMs}ms`));
     }, timeoutMs);
 
-    promise
+    Promise.resolve(promise)
       .then((value) => {
         clearTimeout(timeoutId);
         resolve(value);
